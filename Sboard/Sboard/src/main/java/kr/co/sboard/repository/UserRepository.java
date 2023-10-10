1f16e890ccdcef3a681624b940cea59aef5d1210
ddvvcd <ekfkddjj85@gmail.com>
2023-10-10 18:05:04 +0900

    Sboard 프로젝트

--- Sboard/Sboard/src/main/java/kr/co/sboard/repository/UserRepository.java
+++ Sboard/Sboard/src/main/java/kr/co/sboard/repository/UserRepository.java
@@ -1,2 +1,7 @@
-package kr.co.sboard.repository;public class UserRepository {
+package kr.co.sboard.repository;
+
+import kr.co.sboard.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, String> {
 }
--- Sboard/Sboard/src/main/java/kr/co/sboard/security/SecurityUserService.java
+++ Sboard/Sboard/src/main/java/kr/co/sboard/security/SecurityUserService.java
@@ -1,4 +1,4 @@
-package kr.ch11.security;
+package kr.co.sboard.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -6,8 +6,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import kr.ch11.entity.UserEntity;
-import kr.ch11.repository.UserRepository;
+import kr.co.sboard.entity.UserEntity;
+import kr.co.sboard.repository.UserRepository;
 
 
 @Service
@@ -19,15 +19,16 @@ public class SecurityUserService implements UserDetailsService{
 	
 	@Override //중요한 파트임
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
-		//패스워드에 대한 검사는 이전 컴포넌트(AuthenticationProvider)에서 처리되어 사용자 아이디만 넘어옴
-		UserEntity user = repo.findById(username).get();
-		
-		//사용자 인증객체 생성(세션에 저장)
-		UserDetails userDetails =  MyUserDetails.builder()
-										.user(user)
-										.build();
-									
+
+		// 패스워드에 대한 검사는 이전 컴포넌트(AuthenticationProvider)에서 처리되어 사용자 아이디만 넘어옴
+		UserEntity user = repo.findById(username)
+				.orElseThrow(()->new UsernameNotFoundException(username + " NotFound"));
+
+		// 사용자 인증객체 생성(세션에 저장)
+		UserDetails userDetails = MyUserDetails.builder()
+				.user(user)
+				.build();
+
 		return userDetails;
 	}
 
